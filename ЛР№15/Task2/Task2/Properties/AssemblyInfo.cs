5675180f718154e4f61630bf6d1d539c315954ba
romatvX_ <146443792+romansxxq@users.noreply.github.com>
2025-05-23 00:34:19 +0300

    виконав завдання 2
    виконав завдання 2

--- ЛР№15/Task2/Task2/Properties/AssemblyInfo.cs
+++ ЛР№15/Task2/Task2/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8f410841-5857-4e12-9f60-a8056976f205")]
+[assembly: Guid("5b292100-b56e-4a33-9119-61325a10ead4")]
 
 // Version information for an assembly consists of the following four values:
 //
