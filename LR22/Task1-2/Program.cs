2b516812db8a840bc6598dd7b0f29a22cda32d3b
romatvX_ <asssfsa010@gmail.com>
2025-10-06 23:08:19 +0300

    created LR22 and LR23

---
+++ LR22/Task1-2/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    struct Student
+    {
+        public string Surname;
+        public char Gender;
+        public int BirthYear;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"Stud.dat";
+
+            if (!File.Exists(path))
+            {
+
+                using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Create)))
+                {
+                    Student[] students =
+                    {
+                    new Student { Surname = "Melnychuk", Gender = 'M', BirthYear = 2005 },
+                    new Student { Surname = "Koval", Gender = 'F', BirthYear = 2006 },
+                    new Student { Surname = "Ivanov", Gender = 'M', BirthYear = 2004 },
+                    new Student { Surname = "Shevchenko", Gender = 'M', BirthYear = 2003 },
+                    new Student { Surname = "Petrenko", Gender = 'F', BirthYear = 2005 }
+                };
+                    foreach (var student in students)
+                    {
+                        writer.Write(student.Surname);
+                        writer.Write(student.Gender);
+                        writer.Write(student.BirthYear);
+                    }
+                }
+                Console.WriteLine("File Stud.dat has been created and filled with initial students.\n");
+            }
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.Append), Encoding.UTF8))
+            {
+                Student[] newStudents =
+                {
+                new Student { Surname = "Bondarenko", Gender = 'M', BirthYear = 2005 },
+                new Student { Surname = "Tkachenko", Gender = 'F', BirthYear = 2006 },
+                new Student { Surname = "Kravchenko", Gender = 'M', BirthYear = 2004 }
+            };
+
+                foreach (var s in newStudents)
+                {
+                    writer.Write(s.Surname);
+                    writer.Write(s.Gender);
+                    writer.Write(s.BirthYear);
+                }
+            }
+            Console.WriteLine("New students have been appended to Stud.dat.\n");
+
+            int currentYear = DateTime.Now.Year;
+            int countMan = 0;
+            double sumAge = 0;
+
+            Console.WriteLine("Male students list:\n");
+
+            using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open), Encoding.UTF8))
+            {
+                while (reader.BaseStream.Position < reader.BaseStream.Length)
+                {
+                    Student student = new Student();
+                    student.Surname = reader.ReadString();
+                    student.Gender = reader.ReadChar();
+                    student.BirthYear = reader.ReadInt32();
+
+                    if (student.Gender == 'M')
+                    {
+                        int age = currentYear - student.BirthYear;
+                        Console.WriteLine($"Surname: {student.Surname,-15} | {age} y.o");
+                        countMan++;
+                        sumAge += age;
+                    }
+                }
+            }
+            if (countMan > 0)
+            {
+                double averageAge = sumAge / countMan;
+                Console.WriteLine($"\nAverage year male students: {averageAge:F2} y.o");
+            }
+            else
+            {
+                Console.WriteLine("No male students found.");
+            }
+        }
+    }
+}
---
+++ LR23/Task1/Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task1
+{
+    struct Teams
+    {
+        public string TeamName;
+        public int TeamScore;
+        public Teams(string name, int score)
+        {
+            TeamName = name;
+            TeamScore = score;
+        }
+    }
+    internal class Program
+    {
+        const int NameLength = 20;
+        static void Main(string[] args)
+        {
+            string input = Interaction.InputBox("Enter the number of teams:", "Number of Teams", "3");
+            if (!int.TryParse(input, out int numberOfTeams) || numberOfTeams <= 0)
+            {
+                MessageBox.Show("Invalid number of teams.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            Teams[] teams = new Teams[numberOfTeams];
+            for (int i = 0; i < numberOfTeams; i++)
+            {
+                string name = Interaction.InputBox($"Enter the name of team {i + 1}:", "Team Name", $"Team{i + 1}");
+                if (name.Length > NameLength) name = name.Substring(0, NameLength);
+                string scoreInput = Interaction.InputBox($"Enter the score for team {name}:", "Team Score", "0");
+                if (!int.TryParse(scoreInput, out int score) || score < 0)
+                {
+                    MessageBox.Show("Invalid score! Enter a non-negative integer.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    i--;
+                    continue;
+                }
+                teams[i] = new Teams(name, score);
+            }
+
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "teams.dat");
+            using (BinaryWriter writer = new BinaryWriter(File.Open("teams.dat", FileMode.Create)))
+            {
+                foreach (var team in teams)
+                {
+                    byte[] nameBytes = Encoding.UTF8.GetBytes(team.TeamName.PadRight(NameLength));
+                    writer.Write(nameBytes);
+                    writer.Write(team.TeamScore);
+                }
+            }
+
+            MessageBox.Show($"Teams have been written to {filePath}", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
